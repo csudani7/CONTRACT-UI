adfe7c7c3e27a15076ed0a42f47d5c02b5e7f433
Jenil <jenilghevariya@crystaltechnolab.com>
2022-03-26 16:43:14 +0530

    feat: Added Seprate Download-Contract Page

---
+++ src/pages/download-contract/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import axios from "axios";
+import { PictureAsPdf } from "@mui/icons-material";
+import { saveAs } from "file-saver";
+import { useNavigate } from "react-router-dom";
+
+const DownloadContract = () => {
+  const baseURL = process.env.REACT_APP_API_SERVER;
+  const history = useNavigate();
+
+  const downloadContractPdf = () => {
+    const candiDateData = JSON.parse(localStorage.getItem("candiDateData"));
+
+    axios
+      .post(
+        `http://localhost:8080/candidate/profile/${candiDateData?._id}/download`,
+        {},
+        { responseType: "blob" }
+      )
+      .then((response) => {
+        const blob = new Blob([response?.data], {
+          type: "application/pdf",
+        });
+        saveAs(blob, "contract.pdf");
+        localStorage.clear();
+        history("/");
+      });
+  };
+
+  return (
+    <div className="contractMainDiv">
+        <h1>Download your Contract here....</h1>
+      <div onClick={downloadContractPdf} id="pdfIcon">
+         <PictureAsPdf />
+      </div>
+    </div>
+  );
+};
+
+export default DownloadContract;
