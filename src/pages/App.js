445875a805c5b38c50b03c14fcb189dcb03538b1
Jenil <jenilghevariya@crystaltechnolab.com>
2022-03-26 16:44:17 +0530

    feat: Added Routes

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import UserForm from "./userform";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import "../styles/userForm.css"
+import "../styles/userForm.css";
+import DownloadContract from "./download-contract";
 
 const App = () => {
   return (
-      <UserForm />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<UserForm />} />
+        <Route path="/download-contract" element={<DownloadContract />} />
+      </Routes>
+    </Router>
   );
 };
 
